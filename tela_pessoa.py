4ce88532f0d1551b4603ca2437138eb5757052cf
RayCadu <raynan.araujo2004@gmail.com>
2022-10-28 14:50:35 -0300

    tela pessoa esqueleto

--- tela_pessoa.py
+++ tela_pessoa.py
@@ -3,6 +3,7 @@
 
 from tkinter import *
 
+#def f_tela_pessoa():
 root = Tk()
 root.geometry('300x500')
 root.title('Pessoa')
@@ -22,6 +23,10 @@ texto_nome = Text(root, height = 0.5, width = 20)
 texto_tel = Text(root, height = 0.5, width = 20)
 texto_cpf = Text(root, height = 0.5, width = 20)
 
+#checkbutton
+checkC = Checkbutton(root,text= "SIM")
+checkE = Checkbutton(root,text = "SIM")
+
 #posicionamento textos
 texto_nome.place(relx = 0.4, rely = 0.2, anchor = 'w')
 texto_tel.place(relx = 0.4, rely = 0.3, anchor = 'w')
@@ -30,7 +35,7 @@ texto_cpf.place(relx = 0.4, rely =0.4, anchor = 'w')
 #posicionamento Label
 label_pessoa.place(relx = 0.5, rely = 0.1, anchor = 'n')
 label_nome.place(relx = 0.2, rely = 0.2, anchor = 'w')
-label_tel.place(relx = 0.11, rely = 0.3, anchor = 'w')
+label_tel.place(relx = 0.14, rely = 0.3, anchor = 'w')
 label_cpf.place(relx = 0.25, rely = 0.4, anchor = 'w')
 label_cliente.place(relx = 0.2, rely = 0.5, anchor ='w')
 label_entregador.place(relx = 0.1, rely = 0.6, anchor = 'w')
@@ -39,5 +44,9 @@ label_entregador.place(relx = 0.1, rely = 0.6, anchor = 'w')
 addP.place(relx = 0.5, rely = 0.73, anchor = 'center')
 addE.place(relx = 0.5, rely = 0.83, anchor = 'center')
 addM.place(relx = 0.5, rely = 0.93, anchor = 'center')
+
+#posicionamento checkbutton
+checkC.place(relx = 0.6, rely = 0.5, anchor = 'center')
+checkE.place(relx= 0.6, rely = 0.6, anchor = 'center')
 
 root.mainloop()
