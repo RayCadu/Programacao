a8ed521685ee76026b7a3f3e608b1b287d4aa963
RayCadu <115022802+RayCadu@users.noreply.github.com>
2022-10-28 18:06:34 -0300

    Update tela_menu.py

--- tela_menu.py
+++ tela_menu.py
@@ -1,24 +1,26 @@
 from tkinter import *
-from tela_pessoa import *
+from tela_pessoa import f_tela_pessoa
+from tela_produto import f_tela_produto
+from tela_compra import f_tela_compra
 
 def main():
     root = Tk()
     root.geometry('300x500')
     root.title("Menu")
 
-    label_menu = Label(text="MENU")
+    label_menu = Label(root, text="MENU")
     label_menu.place(relx=0.5, rely=0.1, anchor="center")
 
     addPessoa = Button(root, text="Adicionar Pessoa", command=f_tela_pessoa, background="#808080", foreground="black")
     addPessoa.place(relx=0.5, rely=0.3, anchor="center")
 
-    addProduto = Button(root, text="Adicionar Produto", background="#808080", foreground="black")
+    addProduto = Button(root, text="Adicionar Produto", command=f_tela_produto, background="#808080", foreground="black")
     addProduto.place(relx=0.5, rely=0.4, anchor="center")
 
-    addCompra = Button(root, text="Fazer Compra", background="#808080", foreground="black")
+    addCompra = Button(root, text="Fazer Compra", command= f_tela_compra,  background="#808080", foreground="black")
     addCompra.place(relx=0.5, rely=0.5, anchor="center")
 
-    edit = Button(root, text="Fazer Compra", background="#808080", foreground="black")
+    edit = Button(root, text="Editar", background="#808080", foreground="black")
     edit.place(relx=0.5, rely=0.6, anchor="center")
   
     root.mainloop()
