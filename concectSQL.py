2efaff15c5f48b707a837e9ae2c2199676646dea
RayCadu <raynan.araujo2004@gmail.com>
2022-11-01 20:21:29 -0300

    inserção de dados

--- concectSQL.py
+++ concectSQL.py
@@ -1,21 +1,40 @@
 import psycopg2
+import pandas as pd
 from config import config
 
-def cursor():
+def f_conexao():
     conn = None
     params = config()
     conn = psycopg2.connect(**params)
+
+    return conn
+
+def f_inserirBairro(sql):
+    conn = f_conexao()
     cur = conn.cursor()
-
-    return cur
-
-def main():
-	
-    cur = cursor()
-    cur.execute('SELECT version()')
-    db_version = cur.fetchone()
-    print(db_version)
+    try:
+        cur.execute(sql)
+        id = cur.fetchone()[0]
+        print(id)
+        conn.commit()
+    except(Exception, psycopg2.DatabaseError) as error:
+        print("Error: %s" % error)
+        conn.rollback()
+        cur.close()
+        return 1
     cur.close()
+
+def main():  
+    dados = {
+        "descricao": ['vila velha', 'serra', 'vitória']
+    }
+    df = pd.DataFrame(dados)
+    for i in df.index:
+        sql = '''INSERT INTO BAIRRO(descricao)
+                VALUES('%s') RETURNING codigo;
+                '''%(df['descricao'][i])
+        f_inserirBairro(sql)
+
     return 0
 
 if __name__ == '__main__':main()
