cef44a626881142bf9fa6dfcb65ef1a97ef48b34
RayCadu <raynan.araujo2004@gmail.com>
2022-11-08 10:28:32 -0300

    subindo

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,6 @@
+from concectSQL import f_inserirDados
+
+
 def f_cadastrar_pessoa(nome,cpf,tel,username,senha,logradouro,numero,cep,boxtl,boxcidade,boxbairro,complemento):
     dicp = {}
     dicp["username"] = username.get()
@@ -6,8 +9,7 @@ def f_cadastrar_pessoa(nome,cpf,tel,username,senha,logradouro,numero,cep,boxtl,b
     dicp["cpf"] = cpf.get()
     dicp["senha"] = senha.get()
     dicp["fk_endereco_codigo"] = f_cadastrar_endereco(cep,logradouro,numero,boxbairro,boxcidade,boxtl,complemento)
-    print(dicp)
-    return 0
+    f_inserirDados("PESSOA",dicp,"username")
 
 def f_cadastrar_endereco(cep,logradouro,numero,boxbairro,boxcidade,boxtl,complemento):
     dice = {}
@@ -18,15 +20,19 @@ def f_cadastrar_endereco(cep,logradouro,numero,boxbairro,boxcidade,boxtl,complem
     dice["cidade"] = f_cadastrar_cidade(boxcidade)
     dice["tipo_logradouro"] = f_cadastrar_tl(boxtl)
     dice["complemento"] = complemento.get()
+    f_inserirDados("endereco",dice,"codigo")
 
 def f_cadastrar_bairro(boxbairro):
     dicb = {}
     dicb["descricao"] = boxbairro.get()
+    f_inserirDados("BAIRRO",dicb,"codigo")
 
 def f_cadastrar_cidade(boxcidade):
     dicc = {}
     dicc["descricao"] = boxcidade.get()
+    f_inserirDados("CIDADE",dicc,"codigo")
 
 def f_cadastrar_tl(boxtl):
     dictl = {}
     dictl["descricao"] = boxtl.get()
+    f_inserirDados("TIPO_LOGRADOURO",dictl,"codigo")
